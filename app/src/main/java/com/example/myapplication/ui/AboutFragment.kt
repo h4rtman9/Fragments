1a478d08870ff00e70502bd514476129b52d8034
zafar <zafar@polydev.io>
2023-05-06 21:45:57 +0500

    Menu Completed

---
+++ app/src/main/java/com/example/myapplication/ui/AboutFragment.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+class AboutFragment : Fragment(R.layout.fragment_about) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        }
+    }
---
+++ app/src/main/java/com/example/myapplication/ui/PhotosFragment.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+class PhotosFragment : Fragment(R.layout.fragment_photos) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        }
+    }
